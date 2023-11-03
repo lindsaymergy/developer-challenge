3a36232843f1c4e1fe2b9418320147f4e21a9236
Lindsay Mergy <lindsay.mergy@thearchitects.io>
2023-11-02 21:20:55 -0700

    wip: user can login

--- blueDevChallenger/client/main.js
+++ blueDevChallenger/client/main.js
@@ -1,22 +1,13 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-
 import './main.html';
+import "./login.js";
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+const getUser = () => Meteor.user();
+const isUserLogged = () => !!getUser();
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Template.mainContainer.helpers({
+  isUserLogged() {
+    return isUserLogged();
+  }
 });
